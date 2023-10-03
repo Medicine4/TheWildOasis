68f59faf7a0504c7d0620938decd0323fc5a60d5
medicine <medicinee@qq.com>
2023-10-03 19:33:17 +0800

    useQuery获取cabin数据；cabin页面创建表格；prop-types包定义组件props验证规则，以解决eslint报错

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,11 @@ import AppLayout from "./ui/AppLayout";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
+import PropTypes from "prop-types";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +40,24 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+CabinRow.propTypes = {
+  cabin: PropTypes.object.isRequired,
+};
+
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div> {maxCapacity} 人</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>delete</button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;
