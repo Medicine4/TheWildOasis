aca1484e06c6a755e8609c70853fb968bce4472f
medicine <medicinee@qq.com>
2023-10-20 23:13:10 +0800

    用户登出

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,23 @@
 import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   border-bottom: 1px solid var(--color-grey-100);
-  padding: 30px 40px;
+  padding: 1.2rem 4.8rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 2.4rem;
 `;
 
 function Header() {
-  return <StyledHeader>HEADER</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Logout />
+    </StyledHeader>
+  );
 }
 
 export default Header;
